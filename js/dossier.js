35de934cfdd603a92a6911dc1cbcacdaed618dde
Kirin Patel <kirinpatel@gmail.com>
2017-10-21 14:29:28 -0400

    Baics work for mobile done

--- js/dossier.js
+++ js/dossier.js
@@ -2,3 +2,7 @@ $('.modal').on('hide', function () {
     var leg = $('.videoPlayer').attr("src");
     $('.videoPlayer').attr("src", leg);
 });
+
+if ($('#desktop').is(':visible')) {
+    $('#background_video').html('<video muted autoplay loop align="center"><source src="media/background.mp4" type="video/mp4"></video>');
+}
