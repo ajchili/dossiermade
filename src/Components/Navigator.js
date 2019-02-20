f114046219afcc884ae82cce0aae550e306db447
Elijah <elijahcutler@gmail.com>
2019-02-20 13:32:32 -0500

    Started work on AdminDashboard

--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
+import AdminDashboard from "../Pages/AdminDashboard";
 
 class Navigator extends Component {
   render() {
@@ -10,6 +11,7 @@ class Navigator extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/login" exact component={Login} />
+          <Route path="/dashboard" exact component={AdminDashboard} />
         </Switch>
       </Router>
     );
