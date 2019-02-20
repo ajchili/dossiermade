652e06212c101c7fb503fccefd324ecef0ab7646
Kirin Patel <kirinpatel@gmail.com>
2019-02-20 13:33:43 -0500

    Added navigation based on authentication

--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
+import firebase from "./firebase";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 
 class Navigator extends Component {
+  state = {
+    checkedAuthentication: false,
+    user: null
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ checkedAuthentication: true, user });
+    });
+  }
+
   render() {
+    const { checkedAuthentication, user } = this.state;
+
+    const redirectingAuthenticationRoute = Component =>
+      user ? <Redirect to="/dashboard" /> : <Component />;
+
     return (
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
+          {checkedAuthentication && (
+            <Route
+              path="/login"
+              exact
+              render={() => redirectingAuthenticationRoute(Login)}
+            />
+          )}
         </Switch>
       </Router>
     );
