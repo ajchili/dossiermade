167282ed105518777b56078f64f967f561081120
Kirin Patel <kirinpatel@gmail.com>
2019-02-20 11:33:49 -0500

    Setup firebase component

---
+++ src/Components/firebase.js
@@ -0,0 +1,14 @@
+import firebaseApp from "firebase/app";
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID
+};
+
+let firebase = firebaseApp.initializeApp(config);
+
+export default firebase;
