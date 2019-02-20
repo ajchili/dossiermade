b88a0a21dc4d4d397c854d2e5473030e1f16b2f7
Kirin Patel <kirinpatel@gmail.com>
2019-02-20 13:16:39 -0500

    Setup Login page for authentication handling

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,41 +1,70 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import firebase, { provider } from "../Components/firebase";
 
 class Login extends Component {
   _googleSignIn = () => {
+    const { history } = this.props;
     firebase
       .auth()
       .signInWithPopup(provider)
-      .then(function(result) {
-        var token = result.credential.accessToken;
-        var user = result.user;
+      .then(async result => {
+        const { user } = result;
+        try {
+          let documentSnapshot = await firebase
+            .firestore()
+            .collection("users")
+            .doc(user.uid)
+            .get();
+          if (documentSnapshot.exists && documentSnapshot.data().admin) {
+            history.push("/dashboard");
+          } else {
+            firebase.auth().signOut();
+            alert("Insufficient Permissions");
+            history.push("/");
+          }
+        } catch (err) {
+          console.error(err);
+        }
       })
-      .catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
-      });
+      .catch(() => alert("An unexpected error occurred."));
   };
 
-  styles = {};
-
   render() {
-    console.log(firebase);
     return (
-      <div className="uk-container">
-        <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-text-center">
-          <h3 className="uk-card-title">Administrator Login</h3>
-          <button
-            onClick={this._googleSignIn}
-            className="uk-button uk-button-danger"
-          >
-            LOGIN WITH GOOGLE
-          </button>
+      <div style={styles.fill}>
+        <div style={styles.content}>
+          <div style={styles.contentItems} className="uk-light uk-text-center">
+            <h3>Administrator Login</h3>
+            <button
+              onClick={this._googleSignIn}
+              className="uk-button uk-button-danger"
+            >
+              LOGIN
+            </button>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default Login;
+const styles = {
+  fill: {
+    width: "100%",
+    height: "100vh"
+  },
+  content: {
+    width: "100%",
+    height: "100%",
+    display: "table",
+    position: "relative"
+  },
+  contentItems: {
+    display: "table-cell",
+    verticalAlign: "middle",
+    mixBlendMode: "difference"
+  }
+};
+
+export default withRouter(Login);
