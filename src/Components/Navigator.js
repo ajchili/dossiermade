560b0ddda9b0f0f87d0081b99eb898efe8ccba7a
Kirin Patel <kirinpatel@gmail.com>
2019-02-20 11:39:31 -0500

    Created Navigator component

---
+++ src/Components/Navigator.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+class Navigator extends Component {
+  render() {
+    return <Router />;
+  }
+}
+
+export default Navigator;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Navigator from "./Components/Navigator";
 import "uikit/dist/css/uikit.min.css";
 import "uikit/dist/js/uikit.min.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Navigator />, document.getElementById("root"));
