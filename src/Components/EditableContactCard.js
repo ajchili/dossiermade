5deac07200ae6a9340c3210e83fcfd40ff8fca31
Kirin Patel <kirinpatel@gmail.com>
2019-05-14 12:56:26 -0400

    Setup Contact Information portion of CMS

--- src/Components/EditableContactCard.js
+++ src/Components/EditableContactCard.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 
 export default class EditableContactCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name || "",
+      emailAddress: props.emailAddress || "",
+      phoneNumber: props.phoneNumber || ""
+    };
+  }
+
+  _onValueChange = (key, value) => {
+    let object = {};
+    object[key] = value;
+    this.setState(object);
+  };
+
   render() {
-    const { name, emailAddress, phoneNumber, isEditing } = this.props;
+    const {
+      name,
+      emailAddress,
+      phoneNumber,
+      isEditing,
+      onDelete = () => console.log("Delete action missing!"),
+      onEdit = () => console.log("Edit action missing!"),
+      onCancelEditing = () => console.log("Cancel edit action missing!"),
+      onSave = () => console.log("Save action missing!")
+    } = this.props;
 
     return (
       <div className="uk-card uk-card-body uk-card-default uk-margin-top uk-margin-left uk-margin-right">
         {!isEditing ? (
           <p className="uk-h1">{name}</p>
         ) : (
-          <input className="uk-input" type="text" placeholder="Name" />
+          <input
+            className="uk-input"
+            type="text"
+            placeholder="Name"
+            defaultValue={name}
+            onChange={e => this._onValueChange("name", e.target.value)}
+          />
         )}
         {!isEditing ? (
           <p className="uk-h4">
@@ -23,27 +53,54 @@ export default class EditableContactCard extends Component {
               className="uk-input uk-margin-top"
               type="email"
               placeholder="Email Address"
+              defaultValue={emailAddress}
+              onChange={e =>
+                this._onValueChange("emailAddress", e.target.value)
+              }
             />
             <input
               className="uk-input uk-margin-top"
-              type="number"
+              type="text"
               placeholder="Phone Number"
+              defaultValue={phoneNumber}
+              onChange={e =>
+                this._onValueChange("phoneNumber", e.target.value)
+              }
             />
           </div>
         )}
         {!isEditing ? (
-          <button
-            className="uk-button uk-button-secondary uk-margin-top"
-            style={{ float: "right" }}
-          >
-            Edit
-          </button>
+          <div className="uk-margin-top" style={{ float: "right" }}>
+            <button
+              className="uk-button uk-button-danger uk-margin-right"
+              onClick={onDelete}
+            >
+              Delete
+            </button>
+            <button className="uk-button uk-button-secondary" onClick={onEdit}>
+              Edit
+            </button>
+          </div>
         ) : (
           <div className="uk-margin-top" style={{ float: "right" }}>
-            <button className="uk-button uk-button-danger uk-margin-right">
+            <button
+              className="uk-button uk-button-danger uk-margin-right"
+              onClick={onCancelEditing}
+            >
               Cancel
             </button>
-            <button className="uk-button uk-button-secondary">Save</button>
+            <button
+              className="uk-button uk-button-secondary"
+              onClick={() =>
+                onSave(
+                  this.state.name,
+                  this.state.emailAddress,
+                  this.state.phoneNumber
+                )
+              }
+            >
+              Save
+            </button>
           </div>
         )}
       </div>
--- src/Pages/Admin/ContactInformation.js
+++ src/Pages/Admin/ContactInformation.js
@@ -1,15 +1,162 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-// import firebase, { validateUserPermissions } from "../../Components/firebase";
+import firebase, { validateUserPermissions } from "../../Components/firebase";
 import EditableContactCard from "../../Components/EditableContactCard";
 import Navbar from "../../Components/AdminDashboardNavbar";
 
 class ContactInformation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firebaseAuthListener: null,
+      people: []
+    };
+  }
+
+  componentDidMount() {
+    const { history } = this.props;
+    let firebaseAuthListener = firebase
+      .auth()
+      .onAuthStateChanged(async user => {
+        if (user) {
+          try {
+            let valid = await validateUserPermissions();
+            if (!valid) {
+              throw new Error("Insufficient Permissions!");
+            }
+            this._loadPeople();
+          } catch (err) {
+            // Ignore error and redirect
+            history.push("/login");
+          }
+        } else {
+          history.push("/login");
+        }
+      });
+    this.setState({ firebaseAuthListener });
+  }
+
+  componentWillUnmount() {
+    const { firebaseAuthListener } = this.state;
+    firebaseAuthListener();
+  }
+
+  _loadPeople = async () => {
+    try {
+      let query = await firebase
+        .firestore()
+        .collection("contact-information")
+        .get();
+      let people = query.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+          isEditing: false
+        };
+      });
+      this.setState({ people });
+    } catch (err) {
+      // TODO: Hanle Error
+    }
+  };
+
+  _createPerson = async () => {
+    const { people } = this.state;
+    try {
+      let person = {
+        name: "",
+        email: "info@dossiermade.com",
+        phone: ""
+      };
+      let doc = await firebase
+        .firestore()
+        .collection("contact-information")
+        .add(person);
+      person.id = doc.id;
+      person.isEditing = true;
+      people.push(person);
+      this.setState({ people });
+    } catch (err) {
+      // TODO: Handle Error
+    }
+  };
+
+  _deletePerson = async id => {
+    const { people } = this.state;
+    const { confirm } = window;
+    let person = people.find(person => person.id === id);
+    if (!person) return;
+    else if (!confirm(`Are you sure that you want to delete ${person.name}?`)) {
+      return;
+    }
+    try {
+      await firebase
+        .firestore()
+        .collection("contact-information")
+        .doc(id)
+        .delete();
+      this.setState({
+        people: people.filter(person => person.id !== id)
+      });
+    } catch (err) {
+      // TODO: Handle Error
+    }
+  };
+
+  _savePerson = async (id, newInformation) => {
+    const { people } = this.state;
+    let person = people.find(person => person.id === id);
+    if (!person) return;
+    try {
+      await firebase
+        .firestore()
+        .collection("contact-information")
+        .doc(id)
+        .set(newInformation);
+      Object.keys(newInformation).forEach(key => {
+        person[key] = newInformation[key];
+      });
+      person.isEditing = false;
+      this.setState({ people });
+    } catch (err) {
+      // TODO: Handle Error
+    }
+  };
+
   render() {
+    const { people } = this.state;
     return (
       <div>
         <Navbar />
-        <EditableContactCard />
+        <button
+          className="uk-button uk-margin-top uk-margin-left"
+          onClick={this._createPerson}
+        >
+          Add
+        </button>
+        {people.map(person => {
+          return (
+            <EditableContactCard
+              key={person.id}
+              name={person.name}
+              emailAddress={person.email}
+              phoneNumber={person.phone}
+              isEditing={person.isEditing}
+              onEdit={() => {
+                person.isEditing = true;
+                this.setState({ people });
+              }}
+              onDelete={() => this._deletePerson(person.id)}
+              onCancelEditing={() => {
+                person.isEditing = false;
+                this.setState({ people });
+              }}
+              onSave={(name, email, phone) => {
+                this._savePerson(person.id, { name, email, phone });
+              }}
+            />
+          );
+        })}
       </div>
     );
   }
