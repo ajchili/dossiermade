a4549ce596b887598d9949f5faa17aa80611a4cd
Kirin Patel <kirinpatel@gmail.com>
2019-07-14 10:12:23 -0700

    HomeContentContainer js -> tsx and created AboutCard.tsx

---
+++ web/src/Components/HomeContentContainer.tsx
@@ -0,0 +1,35 @@
+import React, { Component, ReactElement } from "react";
+
+export interface Props {
+  backgroundColor?: "light" | "dark";
+  cardContent: ReactElement;
+  id: string;
+}
+
+export default class HomeContentContainer extends Component<Props> {
+  render() {
+    const { backgroundColor = "light", cardContent, id } = this.props;
+
+    return (
+      <div
+        id={id}
+        className={
+          backgroundColor === "light"
+            ? "uk-background-muted"
+            : "uk-background-secondary"
+        }
+      >
+        <div style={styles.content}>{cardContent}</div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  content: {
+    marginLeft: "15%",
+    marginRight: "15%",
+    paddingTop: "7.5vh",
+    paddingBottom: "7.5vh"
+  }
+};
