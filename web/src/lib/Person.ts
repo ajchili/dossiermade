719eb58720936d1e109f572beecd7ef623c9ad26
Kirin Patel <kirinpatel@gmail.com>
2019-07-14 14:37:56 -0700

    Created Person class

---
+++ web/src/lib/Person.ts
@@ -0,0 +1,69 @@
+import firebase from "./firebase";
+
+export interface PersonSnapshot {
+  name?: string;
+  email?: string;
+  phoneNumber?: string;
+}
+
+export default class Person {
+  private _id: string;
+  private _data: PersonSnapshot;
+  private constructor(id: string, data: PersonSnapshot) {
+    this._id = id;
+    this._data = data;
+  }
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._data.name || "";
+  }
+  get email() {
+    return this._data.email || "";
+  }
+  get phoneNumber() {
+    return this._data.phoneNumber || "";
+  }
+  static async create(data: PersonSnapshot) {
+    let doc = await firebase
+      .firestore()
+      .collection("person")
+      .add(data);
+    return new Person(doc.id, data);
+  }
+  static async getAll(): Promise<Array<Person>> {
+    let people: Array<Person> = [];
+    let query = await firebase
+      .firestore()
+      .collection("person")
+      .get();
+    query.docs.forEach(doc => {
+      people.push(
+        new Person(doc.id, {
+          name: doc.data().name || "",
+          email: doc.data().email || "",
+          phoneNumber: doc.data().phoneNumber || ""
+        })
+      );
+    });
+    return people;
+  }
+  async update(data: PersonSnapshot) {
+    await firebase
+      .firestore()
+      .collection("person")
+      .doc(this._id)
+      .update(data);
+    if (data.name) this._data.name = data.name;
+    if (data.email) this._data.email = data.email;
+    if (data.phoneNumber) this._data.phoneNumber = data.phoneNumber;
+  }
+  async delete() {
+    return await firebase
+      .firestore()
+      .collection("person")
+      .doc(this._id)
+      .delete();
+  }
+}
