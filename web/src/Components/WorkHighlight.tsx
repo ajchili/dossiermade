a7294ac8a50a405172eb266cafcbf3a26bd4f831
Kirin Patel <kirinpatel@gmail.com>
2019-07-15 21:26:41 -0700

    Created WorkHighlight.tsx

---
+++ web/src/Components/WorkHighlight.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Card from "./Card";
+import ContentContainer from "./ContentContainer";
+import Work from "../lib/Work";
+
+export interface Props {
+  work?: Work;
+}
+
+export default class WorkHighlight extends Component<Props> {
+  render() {
+    const { work } = this.props;
+    return (
+      <ContentContainer
+        id="work"
+        backgroundColor="dark"
+        cardContent={
+          <Card
+            title={work ? work.title : "Loading..."}
+            content={
+              <div>
+                {work && (
+                  <iframe
+                    title="video"
+                    src={work.embeddedURL}
+                    style={styles.iframe}
+                  />
+                )}
+              </div>
+            }
+            backgroundColor="dark"
+          />
+        }
+      />
+    );
+  }
+}
+
+const styles = {
+  iframe: {
+    width: "100%",
+    height: "50vh",
+    minHeight: 300,
+    maxHeight: 600
+  }
+};
