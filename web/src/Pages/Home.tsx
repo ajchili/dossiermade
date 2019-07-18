9b8b1fb91342b227bd3e4da598a5131b0cbbced2
Kirin Patel <kirinpatel@gmail.com>
2019-07-17 22:27:58 -0700

    Updated Home and Work to pass props to components

--- web/src/Pages/Home.tsx
+++ web/src/Pages/Home.tsx
@@ -6,36 +6,65 @@ import ContactCard from "../Components/ContactCard";
 import ContentContainer from "../Components/ContentContainer";
 import ServicesCard from "../Components/ServicesCard";
 import WorkCard from "../Components/WorkCard";
+import Person from "../lib/Person";
+import Work from "../lib/Work";
 
 interface State {
   showVideo: boolean;
+  people: Array<Person>;
+  work: Array<Work>;
 }
 
 class Home extends Component<any, State> {
   constructor(props: any) {
     super(props);
     this.state = {
-      showVideo: false
+      showVideo: false,
+      people: Person.getStaticPeople(),
+      work: []
     };
   }
 
-  componentDidMount() {
-    setTimeout(() => this.setState({ showVideo: true }), 1000);
+  async componentDidMount() {
+    try {
+      let work = await Work.getRecent();
+      this.setState({ work });
+    } catch (err) {
+      // Ignore error
+    } finally {
+      this.setState({ showVideo: true });
+      setTimeout(async () => {
+        try {
+          let people = await Person.getAll();
+          this.setState({ people });
+        } catch (err) {
+          // Ignore error
+        }
+      }, 1500);
+    }
   }
-  
+
   render() {
-    const { showVideo } = this.state;
+    const { showVideo, people, work } = this.state;
     return (
       <div>
         <Lander id="lander" showVideo={showVideo} />
         <Navbar />
-        <ContentContainer id="work" cardContent={<WorkCard />} />
+        <ContentContainer
+          id="work"
+          cardContent={
+            <WorkCard
+              work={work}
+              page="home"
+            />
+          }
+        />
         <ContentContainer id="about" cardContent={<AboutCard />} />
         <ContentContainer id="services" cardContent={<ServicesCard />} />
         <ContentContainer
           id="contact"
           backgroundColor="dark"
-          cardContent={<ContactCard />}
+          cardContent={<ContactCard people={people} />}
         />
       </div>
     );
--- web/src/Pages/Work.tsx
+++ web/src/Pages/Work.tsx
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { withRouter, RouteComponentProps } from "react-router-dom";
 import ContentContainer from "../Components/ContentContainer";
 import ContactCard from "../Components/ContactCard";
+import WorkCard from "../Components/WorkCard";
 import WorkHighlight from "../Components/WorkHighlight";
+import Person from "../lib/Person";
 import Work from "../lib/Work";
 
 interface Props extends RouteComponentProps {}
@@ -16,21 +18,27 @@ interface State {
 class WorkPage extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    let state: { work?: Work } = props.location.state || {};
+    let state: { work?: Work; allWork?: Array<Work> } = props.location
+      .state || { work: undefined, allWork: undefined };
     let params: { id?: string } = props.match.params;
-    // Checking for title fixes bug where react state reloads
-    // and changes type of work object.
-    if (state.work === undefined || !state.work.title) {
-      if (params.id) {
-        this._loadSingleWork(params.id);
-      } else {
-        this._loadPaginatedWork();
-      }
+    if (state.work !== undefined && !state.work.title) {
+      state.work = undefined;
+    }
+    if (
+      state.allWork !== undefined &&
+      (!state.allWork.length || !state.allWork[0].title)
+    ) {
+      state.allWork = undefined;
     }
     this.state = {
       type: params.id ? "single" : "all",
-      work: state.work
+      work: state.work,
+      allWork: state.allWork
     };
+    if (params.id) {
+      this._loadSingleWork(params.id);
+    } else if (state.allWork === undefined) {
+    }
     window.scrollTo(0, 0);
   }
 
@@ -48,17 +56,33 @@ class WorkPage extends Component<Props, State> {
     }
   };
 
-  _loadPaginatedWork = async () => {};
-
   render() {
-    const { type, work } = this.state;
+    const { type, work, allWork } = this.state;
     return (
       <div>
         {type === "single" && <WorkHighlight work={work} />}
+        {type === "all" && !!allWork && (
+          <ContentContainer
+            id="content"
+            backgroundColor="dark"
+            cardContent={
+              <WorkCard
+                backgroundColor="dark"
+                bottomContent={
+                  <button className="uk-button uk-button-secondary">
+                    More
+                  </button>
+                }
+                work={allWork || []}
+                page={"other"}
+              />
+            }
+          />
+        )}
         <ContentContainer
           id="content"
           backgroundColor="dark"
-          cardContent={<ContactCard />}
+          cardContent={<ContactCard people={Person.getStaticPeople()} />}
         />
         <div style={{ position: "absolute", top: 0, left: 0 }}>
           <button
