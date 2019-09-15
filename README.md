d8c1e1b9a2607a6d4f3e376f6082bf420beae7c3
Kirin Patel <kirinpatel@gmail.com>
2019-09-15 01:29:29 -0400

    Deleted old website

--- README.md
+++
@@ -1,5 +0,0 @@
-# dossiermade
-A website to host videography work done by @elijahcutler and myself.
-
-
-# All videography and photography work hosted on this website is not to be used without @elijahcutler or my explicit permisson.
--- js/dossier.js
+++
@@ -1,8 +0,0 @@
-$('.modal').on('hide', function () {
-    var leg = $('.videoPlayer').attr("src");
-    $('.videoPlayer').attr("src", leg);
-});
-
-if ($('#desktop').is(':visible')) {
-    $('#background_video').html('<video muted autoplay loop align="center"><source src="media/LazySaturday.mp4" type="video/mp4"></video>');
-}
