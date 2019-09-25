3671f749cc5a8627299c642915cd0ab726bcc3d4
Kirin Patel <kirinpatel@gmail.com>
2019-09-25 11:11:37 -0400

    Added getWorkById function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,24 +11,30 @@ interface Work {
   url: string;
 }
 
+const getWorkFromDoc = (doc: FirebaseFirestore.DocumentSnapshot): Work => {
+  const data = doc.data() || {};
+  const {
+    backgroundImage = "",
+    date = -1,
+    title = "",
+    url = ""
+  } = data;
+  return {
+    backgroundImage,
+    date,
+    id: doc.id,
+    title,
+    url
+  };
+};
+
 const getWorkFromDocs = (docs: FirebaseFirestore.QueryDocumentSnapshot[]): Array<Work> => {
   const work: Array<Work> = [];
   docs.forEach(doc => {
     if (doc.exists) {
-      const {
-        backgroundImage = "",
-        date = -1,
-        title = "",
-        url = ""
-      } = doc.data();
-      if (title.length > 0 && url.length > 0 && backgroundImage.length > 0) {
-        work.push({
-          backgroundImage,
-          date,
-          id: doc.id,
-          title,
-          url
-        });
+      const _work = getWorkFromDoc(doc);
+      if (_work.title.length > 0 && _work.url.length > 0 && _work.backgroundImage.length > 0) {
+        work.push(_work);
       }
     }
   });
@@ -40,4 +46,18 @@ exports.getRecentWork = functions.https.onCall(async (data: any, _: functions.ht
   const query = await admin.firestore().collection("work").orderBy("date", "desc").limit(limit * 2).get();
   const work = getWorkFromDocs(query.docs);
   return work.slice(0, limit);
+});
+
+exports.getWorkById = functions.https.onCall(async (data: any, _: functions.https.CallableContext) => {
+  const { id = null } = data;
+  if (id) {
+    const doc = await admin.firestore().collection("work").doc(id).get();
+    if (doc.exists) {
+      return getWorkFromDoc(doc);
+    } else {
+      throw new Error("Specified work does not exist!");
+    }
+  } else {
+    throw new Error("No id provided, unable to find work!");
+  }
 });
